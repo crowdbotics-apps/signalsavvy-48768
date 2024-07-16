75f35119cc86fe0bf782001d3b4d2e74ea70e66c
Crowdbotics <team@crowdbotics.com>
2024-07-16 06:30:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "signalsavvy-48768",
+  url: "https://signalsavvy-48768.botics.co",
+  api: "https://signalsavvy-48768.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
